41682a7997d452b7217fbe03bcd6cf9e850d44a9
Tommych123 <pmiltsev@mail.ru>
2025-07-28 16:39:41 +0300

    config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,40 @@
 package config
+
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+type Config struct {
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	DBSSLMode  string
+	ServerPort string
+}
+
+func LoadConfig() *Config {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	return &Config{
+		DBHost:     getEnv("DB_HOST", "localhost"),
+		DBPort:     getEnv("DB_PORT", "5432"),
+		DBUser:     getEnv("DB_USER", "postgres"),
+		DBPassword: getEnv("DB_PASSWORD", "postgres"),
+		DBName:     getEnv("DB_NAME", "subscriptions"),
+		DBSSLMode:  getEnv("DB_SSLMODE", "disable"),
+		ServerPort: getEnv("SERVER_PORT", "8080"),
+	}
+}
+
+func getEnv(key, fallback string) string {
+	if val := os.Getenv(key); val != "" {
+		return val
+	}
+	return fallback
+}
